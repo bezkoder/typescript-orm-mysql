626ab8abe6ca6c7ba345c574a6d1bc9241df2ef3
tienbku <tien.ngtruong@gmail.com>
2023-07-11 11:02:54 +0700

    first commit

---
+++ src/config/db.config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "123456",
+  DB: "testdb",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+export const dialect = "mysql";
